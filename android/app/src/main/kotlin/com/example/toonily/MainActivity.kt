ff4e899cd3afcd3af5b351605df04ceea00fd0ba
catina2 <caterina2@tkhplanesw.comm>
2022-09-07 17:11:00 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/toonily/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.toonily
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    // ignore: prefer_const_constructors
+    return WebView(
+      initialUrl: "https://toonily.com",
+      javascriptMode: JavascriptMode.unrestricted,
+    );
+  }
+}
